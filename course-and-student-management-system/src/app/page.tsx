64474c0aeea8228070e3a8933921e9598c3ce9f3
Andres-PeMu <andres.pena.m15@gmail.com>
2023-04-21 08:47:47 -0500

    probando funconalidad redux

--- course-and-student-management-system/src/app/page.tsx
+++ course-and-student-management-system/src/app/page.tsx
@@ -1,13 +1,15 @@
+import { Provider } from 'react-redux';
+import store from './utils/redux/store';
+
 import Home from './pages/Home/page';
 import Layout from './pages/layout';
 
 export default function MyApp() {
-
   return (
-    <>
+    <Provider store={store}>
       <Layout>
         <Home></Home>
       </Layout>
-    </>
+    </Provider>
   );
 }
