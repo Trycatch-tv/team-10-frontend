085c63abfe559997d6a4b35e6a9c6a6294efc43a
Juan Pablo Rodriguez <96890522+krosul@users.noreply.github.com>
2023-04-30 19:07:36 -0500

    fixes

--- course-and-student-management-system/src/app/hooks/UserState.tsx
+++ course-and-student-management-system/src/app/hooks/UserState.tsx
@@ -24,7 +24,7 @@ const AppStateProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
       email: '',
       phone: '',
       role: '',
-      isAuthenticated: true,
+      isAuthenticated: false,
     });
   };
   return (
